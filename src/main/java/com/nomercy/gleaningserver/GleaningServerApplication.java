4540231cb9a7c959d1497b72e7acd341372be92d
BigSaltyFish <2983483048@qq.com>
2019-10-11 21:02:16 +0800

    :tada: the initial commit

---
+++ src/main/java/com/nomercy/gleaningserver/GleaningServerApplication.java
@@ -0,0 +1,13 @@
+package com.nomercy.gleaningserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GleaningServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GleaningServerApplication.class, args);
+	}
+
+}
